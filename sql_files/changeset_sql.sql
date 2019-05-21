2a84d70d124d20d4e244d4908adc83e5a61ea2b1
aishwarya.gaud <aishwarya.gaud@iauro.com>
2019-05-22 00:04:51 +0530

    liquibase-changelog.xml updated

--- sql_files/changeset_sql.sql
+++ sql_files/changeset_sql.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 --changeset xameeramir:create-test-table
-CREATE TABLE IF NOT EXISTS testTable(
+CREATE TBLE IF NOT EXISTS testTable(
 columnName1 VARCHAR (355)
 );
 --rollback DROP TABLE
