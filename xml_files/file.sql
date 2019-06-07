63315ca1fbcde72c019ebed8fc8c036499912e18
aishwarya.gaud <aishwarya.gaud@iauro.com>
2019-06-07 16:59:10 +0530

    xml_files/file.sql updated

--- xml_files/file.sql
+++ xml_files/file.sql
@@ -4,3 +4,7 @@ select * from person;
 select * from state;
 select * from testtable;
 select column_name,data_type,column_default,is_nullable from information_schema.columns where table_schema = 'public' and table_name = 'department';
+select column_name,data_type,column_default,is_nullable from information_schema.columns where table_schema = 'public' and table_name = 'employee';
+select column_name,data_type,column_default,is_nullable from information_schema.columns where table_schema = 'public' and table_name = 'person';
+select column_name,data_type,column_default,is_nullable from information_schema.columns where table_schema = 'public' and table_name = 'state';
+select column_name,data_type,column_default,is_nullable from information_schema.columns where table_schema = 'public' and table_name = 'testtable';
