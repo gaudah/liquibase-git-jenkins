68896c8285722953bbe4c2887e94904dc0d4288a
aishwarya.gaud <aishwarya.gaud@iauro.com>
2019-06-07 17:13:08 +0530

    xml_files/file.sql updated

---
+++ xml_files/sql_commands.sql
@@ -0,0 +1,10 @@
+select * from department;
+select * from employee;
+select * from person;
+select * from state;
+select * from testtable;
+select column_name,data_type,column_default,is_nullable from information_schema.columns where table_schema = 'public' and table_name = 'department';
+select column_name,data_type,column_default,is_nullable from information_schema.columns where table_schema = 'public' and table_name = 'employee';
+select column_name,data_type,column_default,is_nullable from information_schema.columns where table_schema = 'public' and table_name = 'person';
+select column_name,data_type,column_default,is_nullable from information_schema.columns where table_schema = 'public' and table_name = 'state';
+select column_name,data_type,column_default,is_nullable from information_schema.columns where table_schema = 'public' and table_name = 'testtable';
