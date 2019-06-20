017362e3609399ad771724252837f26ba255b8ce
aishwarya.gaud <aishwarya.gaud@iauro.com>
2019-06-20 17:15:06 +0530

    xml_files/sql_commands.sql reverted

--- xml_files/sql_commands.sql
+++ xml_files/sql_commands.sql
@@ -2,7 +2,6 @@ select * from department;
 select * from employee;
 select * from person;
 select * from testtable;
-select * from testtable17;
 
 select column_name,data_type,column_default,is_nullable from information_schema.columns where table_schema = 'public' and table_name = 'department';
 select column_name,data_type,column_default,is_nullable from information_schema.columns where table_schema = 'public' and table_name = 'employee';
